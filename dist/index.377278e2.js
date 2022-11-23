5eaa6a538b001c25babdc2b23f0c3bdd495b2963
unknown <martinratm@gmail.com>
2022-11-23 19:22:43 -0300

    [ADD] Updated files

--- dist/index.377278e2.js
+++
@@ -1,9 +0,0 @@
-//import { getPrice, getCoin } from "./coingecko";
-function getCoin() {
-    let coinID = document.getElementById("coinID");
-    console.log(coinID);
-    let coinName = coinID.value;
-    return coinName;
-} // console.log(getPrice("bitcoin", "ars"))
-
-//# sourceMappingURL=index.377278e2.js.map
